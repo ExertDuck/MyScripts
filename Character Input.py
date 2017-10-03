55af4bf236eb62822dc045c858181ff354442fc1
Ricky Tavares <tavaresricky@gmail.com>
2017-10-03 20:57:09 +0100

    Initial commit

---
+++ Character Input.py
@@ -0,0 +1,15 @@
+import datetime
+
+name = raw_input("What is your name? ")
+age = int(raw_input("How old are you? "))
+num = int(raw_input("Enter a number from 0 - 9: ")) # will print the final answer this number of times
+
+while num < 0 or num > 20:	# loops until a valid number is entered
+	num = int(raw_input("Invalid number. Please enter a number between 0 - 20: "))
+
+year = datetime.datetime.now().year
+year_100 = year + 100 - age
+
+while num > 0:
+	print("Hi, " + name + "! You'll be 100 years old in " + str(year_100))
+	num -= 1
---
+++ dictionary.py
@@ -0,0 +1,5 @@
+superheroes = {}
+superheroes["Superman"] = 1
+superheroes["Batman"] = 2
+superheroes["Spiderman"] = 3
+print(superheroes)
